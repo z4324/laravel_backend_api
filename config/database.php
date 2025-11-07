beea441e52a78b6870c03c6f86afe2b8f7d554a6
jz8344 <jose.zanabria.23e@utzmg.edu.mx>
2025-11-06 21:29:08 -0600

    Upgrade database connection to use SSL mode

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('PGSSLMODE', 'prefer'),
         ],
 
     ],
